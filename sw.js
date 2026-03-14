95e41be9e746527b34d4ca8156977d3ebd79fb87
abdul6teen-max <abdul6teen@gmail.com>
2026-03-14 15:19:04 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -11,7 +11,26 @@ self.addEventListener('activate', e => {
   e.waitUntil(clients.claim());const CACHE = 'mnn-v1';
 
 self.addEventListener('install', e => {
-  self.skipWaiting();
+  self.skipWaiting();const CACHE_NAME = 'mnn-japanese-v1';
+
+self.addEventListener('install', e => {
+  e.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => new Response('', {status: 200}))
+  );
+});
+
 });
 
 self.addEventListener('activate', e => {
